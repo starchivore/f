961154717859b05387c64d27e59d51cb44b0272b
starchivore <briefcaseaod@yahoo.com>
2022-09-09 05:53:49 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -29,7 +29,7 @@ FFMPEG_CONFIGURE_FLAGS=(
     --disable-filter=scale_cuda
     --enable-small
     --disable-filters
-    --enable-filter='*null*,afade,*fifo,*format,*resample,aeval,allrgb,allyuv,atempo,pan,*bars,color,*key,crop,draw*,eq*,framerate,scale,volume,test*'
+    --enable-filter='*null*,afade,*fifo,*format,*resample,aeval,atempo,eq*,framerate,scale,volume,test*'
     --disable-decoders
     --enable-decoder='aac,aac_latm,ac3,alac,als,ape,atrac1,atrac3,eac3,flac,gsm,gsm_ms,mp1,mp1float,mp2,mp2float,mp3,mp3adu,mp3adufloat,mp3float,mp3on4,mp3on4float,mpc7,mpc8,opus,ra_144,ra_288,ralf,shorten,tak,tta,vorbis,wavpack,wmalossless,wmapro,wmav1,wmav2,wmavoice,pcm_alaw,pcm_bluray,pcm_dvd,pcm_f32be,pcm_f32le,pcm_f64be,pcm_f64le,pcm_lxf,pcm_mulaw,pcm_s8,pcm_s8_planar,pcm_s16be,pcm_s16be_planar,pcm_s16le,pcm_s16le_planar,pcm_s24be,pcm_s24daud,pcm_s24le,pcm_s24le_planar,pcm_s32be,pcm_s32le,pcm_s32le_planar,pcm_u8,pcm_u16be,pcm_u16le,pcm_u24be,pcm_u24le,pcm_u32be,pcm_u32le,pcm_zork,dsd_lsbf,dsd_msbf,dsd_lsbf_planar,dsd_msbf_planar'
     --disable-demuxers
