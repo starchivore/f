d6aba2b560062fbaf8497c1cf8e21a2793977dac
starchivore <briefcaseaod@yahoo.com>
2022-09-09 07:51:53 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -7,7 +7,8 @@ FFMPEG_TARBALL_URL=https://github.com/ffmpeg/ffmpeg/tarball/release/$FFMPEG_VERS
 FFMPEG_CONFIGURE_FLAGS=(
     --disable-doc
     --disable-debug
-    --enable-shared
+    --disable-shared
+    --enable-static
     --enable-ffmpeg
     --disable-ffplay
     --disable-protocols
@@ -35,6 +36,4 @@ FFMPEG_CONFIGURE_FLAGS=(
     --enable-demuxer='image2,aac,ac3,aiff,ape,asf,au,avi,flac,flv,matroska,mov,m4v,mp3,mpc,mpc8,ogg,pcm_alaw,pcm_mulaw,pcm_f64be,pcm_f64le,pcm_f32be,pcm_f32le,pcm_s32be,pcm_s32le,pcm_s24be,pcm_s24le,pcm_s16be,pcm_s16le,pcm_s8,pcm_u32be,pcm_u32le,pcm_u24be,pcm_u24le,pcm_u16be,pcm_u16le,pcm_u8,rm,shorten,tak,tta,wav,wv,xwma,dsf'
     --disable-parsers
     --enable-parser='aac,aac_latm,ac3,cook,dca,flac,gsm,mpegaudio,tak,vorbis'
-    --disable-encoders
-    --disable-muxers
 )
