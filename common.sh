7715f0fda879efc2a1a23bc758f7deaafe7fca79
starchivore <briefcaseaod@yahoo.com>
2022-09-09 00:10:36 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -17,7 +17,13 @@ FFMPEG_CONFIGURE_FLAGS=(
     --disable-postproc
     --enable-pic
     --disable-filter=scale_cuda
-    --disable-outdevs
-    --disable-muxers
-    --disable-encoders
+    --enable-small
+    --disable-filters
+    --enable-filter='*null*,afade,*fifo,*format,*resample,aeval,allrgb,allyuv,atempo,pan,*bars,color,*key,crop,draw*,eq*,framerate,*_qsv,*_vaapi,*v4l2*,hw*,scale,volume,test*'
+    --disable-decoders
+    --enable-decoder='*sub*,movtext,*web*,aac*,*ac3*,alac*,ape,ass,av1*,cc_dec,cook,dca,dnxhd,eac3*,exr,truehd,ff*,*yuv*,flv,flac,gif,h26[3-4]*,hevc*,hap,mp[1-3]*,prores,*peg*,mlp,mpl2,nellymoser,opus,pcm*,qtrle,*png*,tiff,rawvideo,rv*,sami,srt,ssa,v210*,vc1*,vorbis,vp[6-9]*,wm*,wrapped_avframe'
+    --disable-demuxers
+    --enable-demuxer='*sub*,*ac3,*ac,*peg*,*web*,ape,ass,avi,concat,dnxhd,dts*,*dash*,*flv,gif,hls,h264,hevc,kux,xv,matroska,mlv,mov,mp3,mxf,nsv,nut,ogg,pcm*,rawvideo,rt*p,spdif,srt,vc1,v210*,wav,*pipe,image2'
+    --disable-parsers
+    --enable-parser='*sub*,aac*,ac3,cook,dnxhd,flac,h26[3-4],hevc,m*,opus,rv*,vc1,vorbis,vp[8-9]'
 )
