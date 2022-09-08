2bfc4f44773bc7c0dabd615784b888cf0f824705
starchivore <briefcaseaod@yahoo.com>
2022-09-08 17:57:49 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -7,22 +7,30 @@ FFMPEG_TARBALL_URL=https://github.com/ffmpeg/ffmpeg/tarball/release/$FFMPEG_VERS
 FFMPEG_CONFIGURE_FLAGS=(
     --disable-shared
     --enable-static
-    --enable-pic
-
-    --disable-doc
     --disable-debug
+    --disable-doc
+
     --disable-avdevice
     --disable-swscale
-    --disable-programs
-    --enable-rdft
-    --enable-ffmpeg
-    --enable-ffprobe
-    --disable-network
-    --disable-muxers
-    --disable-demuxers
+    --disable-postproc
+    --disable-ffmpeg
+    --disable-ffplay
+
+    --disable-iconv
     --disable-zlib
     --disable-bzlib
-    --disable-iconv
+    --disable-lzma
+    --disable-sdl2
+    --disable-schannel
+    --disable-securetransport
+    --disable-xlib
+
+    --disable-muxers
+    --disable-demuxers
+    --disable-hwaccels
+    --disable-d3d11va
+    --disable-nvenc
+    --disable-dxva2
     --disable-bsfs
     --disable-filters
     --disable-parsers
@@ -30,25 +38,7 @@ FFMPEG_CONFIGURE_FLAGS=(
     --disable-outdevs
     --disable-encoders
     --disable-decoders
-    --disable-hwaccels
-    --disable-nvenc
-    --disable-videotoolbox
-    --disable-audiotoolbox
-
-    --disable-filters
-    --enable-filter=aformat
-    --enable-filter=anull
-    --enable-filter=atrim
-    --enable-filter=format
-    --enable-filter=null
-    --enable-filter=setpts
-    --enable-filter=trim
-
-    --disable-protocols
-    --enable-protocol=file
-    --enable-protocol=pipe
-    --enable-protocol=http
-    --enable-protocol=https
+    --disable-bsfs
 
     --enable-demuxer=image2
     --enable-demuxer=aac
@@ -64,29 +54,9 @@ FFMPEG_CONFIGURE_FLAGS=(
     --enable-demuxer=mov
     --enable-demuxer=m4v
     --enable-demuxer=mp3
-    --enable-demuxer=mpc
-    --enable-demuxer=mpc8
+    --enable-demuxer=mpc*
     --enable-demuxer=ogg
-    --enable-demuxer=pcm_alaw
-    --enable-demuxer=pcm_mulaw
-    --enable-demuxer=pcm_f64be
-    --enable-demuxer=pcm_f64le
-    --enable-demuxer=pcm_f32be
-    --enable-demuxer=pcm_f32le
-    --enable-demuxer=pcm_s32be
-    --enable-demuxer=pcm_s32le
-    --enable-demuxer=pcm_s24be
-    --enable-demuxer=pcm_s24le
-    --enable-demuxer=pcm_s16be
-    --enable-demuxer=pcm_s16le
-    --enable-demuxer=pcm_s8
-    --enable-demuxer=pcm_u32be
-    --enable-demuxer=pcm_u32le
-    --enable-demuxer=pcm_u24be
-    --enable-demuxer=pcm_u24le
-    --enable-demuxer=pcm_u16be
-    --enable-demuxer=pcm_u16le
-    --enable-demuxer=pcm_u8
+    --enable-demuxer=pcm*
     --enable-demuxer=rm
     --enable-demuxer=shorten
     --enable-demuxer=tak
@@ -95,83 +65,37 @@ FFMPEG_CONFIGURE_FLAGS=(
     --enable-demuxer=wv
     --enable-demuxer=xwma
     --enable-demuxer=dsf
+    --enable-demuxer=dts
+    --enable-demuxer=truehd
 
-    --enable-decoder=aac
-    --enable-decoder=aac_latm
+    --enable-decoder=aac*
     --enable-decoder=ac3
     --enable-decoder=alac
     --enable-decoder=als
     --enable-decoder=ape
-    --enable-decoder=atrac1
-    --enable-decoder=atrac3
+    --enable-decoder=atrac*
     --enable-decoder=eac3
     --enable-decoder=flac
-    --enable-decoder=gsm
-    --enable-decoder=gsm_ms
-    --enable-decoder=mp1
-    --enable-decoder=mp1float
-    --enable-decoder=mp2
-    --enable-decoder=mp2float
-    --enable-decoder=mp3
-    --enable-decoder=mp3adu
-    --enable-decoder=mp3adufloat
-    --enable-decoder=mp3float
-    --enable-decoder=mp3on4
-    --enable-decoder=mp3on4float
-    --enable-decoder=mpc7
-    --enable-decoder=mpc8
+    --enable-decoder=gsm*
+    --enable-decoder=mp1*
+    --enable-decoder=mp2*
+    --enable-decoder=mp3*
+    --enable-decoder=mpc*
     --enable-decoder=opus
-    --enable-decoder=ra_144
-    --enable-decoder=ra_288
+    --enable-decoder=ra*
     --enable-decoder=ralf
     --enable-decoder=shorten
     --enable-decoder=tak
     --enable-decoder=tta
     --enable-decoder=vorbis
     --enable-decoder=wavpack
-    --enable-decoder=wmalossless
-    --enable-decoder=wmapro
-    --enable-decoder=wmav1
-    --enable-decoder=wmav2
-    --enable-decoder=wmavoice
+    --enable-decoder=wma*
+    --enable-decoder=pcm*
+    --enable-decoder=dsd*
+    --enable-decoder=truehd
+    --enable-decoder=mjpeg
 
-    --enable-decoder=pcm_alaw
-    --enable-decoder=pcm_bluray
-    --enable-decoder=pcm_dvd
-    --enable-decoder=pcm_f32be
-    --enable-decoder=pcm_f32le
-    --enable-decoder=pcm_f64be
-    --enable-decoder=pcm_f64le
-    --enable-decoder=pcm_lxf
-    --enable-decoder=pcm_mulaw
-    --enable-decoder=pcm_s8
-    --enable-decoder=pcm_s8_planar
-    --enable-decoder=pcm_s16be
-    --enable-decoder=pcm_s16be_planar
-    --enable-decoder=pcm_s16le
-    --enable-decoder=pcm_s16le_planar
-    --enable-decoder=pcm_s24be
-    --enable-decoder=pcm_s24daud
-    --enable-decoder=pcm_s24le
-    --enable-decoder=pcm_s24le_planar
-    --enable-decoder=pcm_s32be
-    --enable-decoder=pcm_s32le
-    --enable-decoder=pcm_s32le_planar
-    --enable-decoder=pcm_u8
-    --enable-decoder=pcm_u16be
-    --enable-decoder=pcm_u16le
-    --enable-decoder=pcm_u24be
-    --enable-decoder=pcm_u24le
-    --enable-decoder=pcm_u32be
-    --enable-decoder=pcm_u32le
-    --enable-decoder=pcm_zork
-    --enable-decoder=dsd_lsbf
-    --enable-decoder=dsd_msbf
-    --enable-decoder=dsd_lsbf_planar
-    --enable-decoder=dsd_msbf_planar
-
-    --enable-parser=aac
-    --enable-parser=aac_latm
+    --enable-parser=aac*
     --enable-parser=ac3
     --enable-parser=cook
     --enable-parser=dca
@@ -180,6 +104,4 @@ FFMPEG_CONFIGURE_FLAGS=(
     --enable-parser=mpegaudio
     --enable-parser=tak
     --enable-parser=vorbis
-    
-    --enable-openssl
 )
